ed16f955675ae0936bd28db0e660ecd46164215b
ramandeepsingh <ramandeep.singh.in@gmail.com>
2022-04-04 14:59:43 +0530

    view layer and customer banner

---
+++ src/main/java/guru/springframework/spring5jokesapp/controllers/JokesController.java
@@ -0,0 +1,22 @@
+package guru.springframework.spring5jokesapp.controllers;
+
+import guru.springframework.spring5jokesapp.services.JokesService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokesController {
+
+    public final JokesService jokesService;
+
+    public JokesController(JokesService jokesService) {
+        this.jokesService = jokesService;
+    }
+
+    @RequestMapping({"/",""})
+    public String showJoke(Model model){
+        model.addAttribute("joke",jokesService.getJoke());
+        return "index";
+    }
+}
